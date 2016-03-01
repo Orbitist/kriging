d5106a38c0691e3794cd89da11dea5d47618d1c2
Nicholas Gunner <gunnernr@gmail.com>
2016-03-01 11:17:20 -0500

    initial upload

---
+++ cornell/measurements.js
@@ -0,0 +1,9 @@
+/* Random (log) black carbon data */
+var longitude = longs;
+
+var latitude = lats;
+
+var response = ndvi;
+
+var longrange = [longitude.min(), longitude.max()];
+var latrange = [latitude.min(), latitude.max()];
